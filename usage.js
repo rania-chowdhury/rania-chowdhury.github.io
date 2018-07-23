0eee1ce50cd3a21dba05e6240cffcface317eaf7
Chowdhury <rania.chowdhury@tallan.com>
2018-07-23 08:03:32 -0400

    index fix

--- usage.js
+++ usage.js
@@ -11,7 +11,7 @@ function openTab(event, tabName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    // show the current tab, and add an "active class to the button that opened the tab"
+    // show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     event.currentTarget.className += " active";
 }
