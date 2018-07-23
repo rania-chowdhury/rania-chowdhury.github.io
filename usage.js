f5c9f27a9698c56fccbf2cb09c3738da18c0ea27
Chowdhury <rania.chowdhury@tallan.com>
2018-07-23 14:15:35 -0400

    another fix for page content

--- usage.js
+++ usage.js
@@ -11,17 +11,19 @@ function openTab(tabName, pageName, element, color) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
-    // get all elements with class="content" and hide them
-    pagecontent = document.getElementsByClassName("content");
-    for(i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none"
-    }
     // show specific tab content
     document.getElementById(tabName).style.display = "block";
-    // show specific page content
-    document.getElementById(pageName).style.display = "block";
     // add specific color to button used to open tab content
     element.style.backgroundColor = color;
+
+    // get all elements with class="content" and hide them
+    pagecontent = document.getElementsByClassName("content");
+    for(i = 0; i < pagecontent.length; i++) {
+        pagecontent[i].style.display = "none"
+    }
+    // show specific page content
+    document.getElementById(pageName).style.display = "block";
+
 }
 // get element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
