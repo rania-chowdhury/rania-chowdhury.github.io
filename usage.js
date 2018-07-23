f528a495968f451e9e88437d2ef5c91ca8ca8e3b
Chowdhury <rania.chowdhury@tallan.com>
2018-07-23 07:50:30 -0400

    commit for tabs

---
+++ usage.js
@@ -0,0 +1,17 @@
+function openTab(event, tabName) {
+    // declare all variables
+    var i, tabcontent, tablinks;
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // show the current tab, and add an "active class to the button that opened the tab"
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
