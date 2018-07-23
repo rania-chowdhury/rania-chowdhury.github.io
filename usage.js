7f435618cf9602bb75bafdb92f6169778ee8d0dc
Chowdhury <rania.chowdhury@tallan.com>
2018-07-23 15:44:55 -0400

    possible fix

--- usage.js
+++ usage.js
@@ -11,6 +11,15 @@ function openTab(tabName, element, color) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    // remove the background color of all tablinks/buttons
+    links = document.getElementsByClassName("contentlink");
+    for (i = 0; i < links.length; i++) {
+        links[i].style.backgroundColor = "";
+    }
     // show specific tab content
     document.getElementById(tabName).style.display = "block";
     // add specific color to button used to open tab content
@@ -20,12 +29,21 @@ function openPage(pageName, element, color) {
     // declare all variables
     var i, tabcontent, tablinks;
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("content");
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    // remove the background color of all tablinks/buttons
+    links = document.getElementsByClassName("contentlink");
+    for (i = 0; i < links.length; i++) {
+        links[i].style.backgroundColor = "";
+    }
+    tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     // remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("contentlink");
+    tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
