30528abba8f5c1f750142fd580f1529f4bdceeed
Chowdhury <rania.chowdhury@tallan.com>
2018-07-23 15:36:10 -0400

    possible fix

--- usage.js
+++ usage.js
@@ -20,12 +20,12 @@ function openPage(pageName, element, color) {
     // declare all variables
     var i, tabcontent, tablinks;
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     // remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("contentlink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
