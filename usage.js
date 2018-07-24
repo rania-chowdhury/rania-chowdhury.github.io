fa4c4233962b5453b9c493985217458f65d8c202
Chowdhury <rania.chowdhury@tallan.com>
2018-07-24 08:58:17 -0400

    slideshow attempt

--- usage.js
+++ usage.js
@@ -35,3 +35,26 @@ function openPage(pageName, element, color) {
     element.style.backgroundColor = color;
 }
 document.getElementById("defaultOpen").click();
+
+// slideshow logic
+var slideIndex = 1;
+showSlide(slideIndex);
+//next/prev controls
+function shiftSlide(n) {
+    showSlide(slideIndex += n);
+}
+function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides")
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
